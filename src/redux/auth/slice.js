2dab3f61ec3a1456a8d66d23770adeef29922719
Levina Tani <edelaida@gmail.com>
2024-05-19 22:17:00 +0300

    proba

---
+++ src/redux/auth/slice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { logIn, logOut, register } from "./operations";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: {
+      name: null,
+      email: null,
+    },
+    token: null,
+    isLoggedIn: false,
+  },
+  extraReducers: (builder) =>
+    builder
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logIn.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logOut.fulfilled, (state) => {
+        state.user = {
+          name: null,
+          email: null,
+        };
+        state.token = null;
+        state.isLoggedIn = false;
+      }),
+});
+//cjkh
+// export default authSlice.reducer;
+export const authReducer = authSlice.reducer;
